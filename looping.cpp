7e27ccd88c016bdaa7f2c171d91ae7a7060ebbc6
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-03-31 15:59:29 +0700

    1

--- looping.cpp
+++ looping.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+
+int main( ){
+    int x;
+   
+    //Perulangan dengan FOR
+    //mencetak tulisan seabnyak 5 kali
+    cout << "PERULANGAN FOR" << endl
+    for(int i=0; i <= 4; i++){
+        cout << "Teknologi Informasi UMY" << endl;
+    }
+    cout << endl;
+    //untuk mendapatkanb urutan angka acak yg berbeda
+    srand(time(0));
+    
+}
